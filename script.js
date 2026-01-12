13bbfe5c6ab0389751ef86a1d83b62716c4692dc
Isaac <88962878+poseiddonn@users.noreply.github.com>
2026-01-12 16:50:16 -0500

    Refactor HTML structure for better readability and update JavaScript prompts for clarity

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function handleInput() {
   var name = prompt("Enter your name: ");
-  var hobby = prompt("what's your favorite hobby: ")
+  var hobby = prompt("what's your favorite hobby: ");
   var msg = `Your Name is ${name}, and your favorite hobby is ${hobby}!`;
+  var loc = prompt("Where do you live: ");
+  var locData = `You live in ${loc}`;
   document.getElementById("opArea").innerHTML = msg;
+  document.getElementById("locArea").innerHTML= locData;
 }
